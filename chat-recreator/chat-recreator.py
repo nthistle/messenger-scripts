575cade82a7f267a037bb8c72daa8f6227d0e1dc
Neil Thistlethwaite <neilt1111@yahoo.com>
2017-10-23 20:02:47 -0400

    Start chat-recreator

---
+++ chat-recreator/chat-recreator.py
@@ -0,0 +1,10 @@
+## Chat Re-Creator v1.0
+## @author Neil Thistlethwaite
+import getpass
+
+print("Chat Re-Creator v1.0")
+print("(C) 2017 Neil Thistlethwaite")
+print("")
+print("Please Login")
+email = input("Email: ")
+password = getpass.getpass("Password: ")
