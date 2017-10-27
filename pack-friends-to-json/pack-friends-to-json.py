829e28e6bb4735984a39aa932d2b94515edc54ff
Ryan <rcpope7@gmail.com>
2017-10-26 20:59:38 -0400

    Added pack-friends-to-json
    pack-friends-to-json creates a JSONified lookup table to increase efficiency and ease of converting names to user IDs.

---
+++ pack-friends-to-json/pack-friends-to-json.py
@@ -0,0 +1,19 @@
+from fbchat import Client
+from fbchat.models import *
+import json
+
+client = Client("<username>", "<password>")
+
+table = {}
+users = client.fetchAllUsers()
+
+for user in users:
+	#converts from special type to direct string lookup
+	try:
+		table[str(user.name)] = user.uid
+	except UnicodeEncodeError:
+		table[user.name] = user.uid
+
+# use this lookup table in another program by reading the file and calling json.loads()
+f = open("namesjson.txt", "w")
+f.write(json.dumps(table))
